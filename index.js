8385ec3a066919c71d596a9c3be595ee3330c20c
jdhawks2132 <90884050+jdhawks2132@users.noreply.github.com>
2021-10-27 20:27:31 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// let banner = document.querySelector('#banner');
+// const hawksLogoUrl = './images/Hawks Design Black.png'
+// const bannerUrl = './images/feat2.png'
+
+
+// banner.addEventListener('mouseover', ()=> {
+//     banner.src=hawksLogoUrl})
